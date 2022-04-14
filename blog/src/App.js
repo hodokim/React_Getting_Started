db530317818f749a0da192aba81d4eb66dfc4cd3
hodokim <hodo323@naver.com>
2022-04-14 22:53:28 +0900

    getting started React

---
+++ blog/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="black-nav">
+        <div>나만의 로그</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
